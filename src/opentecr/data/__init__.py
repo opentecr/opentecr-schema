c55f33de8cefbc56ba0c3a56f8758ea088f97523
Robert Giessmann <rgiessmann@gmail.com>
2022-03-16 11:48:43 +0100

    sets up traffic branch

--- src/opentecr/data/__init__.py
+++
@@ -1,6 +0,0 @@
-from .annotation import Annotation
-from .contributor import Contributor
-from .literature_reference import LiteratureReference
-from .metabolite import Metabolite
-from .representative_reaction import RepresentativeReaction
-from .curated_measurement import CuratedMeasurement
