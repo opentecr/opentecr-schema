add0f438728fe1ab4d945cc17511535c1aa397b1
teddygroves <groves.teddy@gmail.com>
2023-12-13 16:05:49 +0100

    Update json schema script and add test for it

--- scripts/json_schema.py
+++ scripts/json_schema.py
@@ -1,22 +1,23 @@
+"""Write the CuratedMeasurement schema to a json file."""
+
 import json
+import logging
 import sys
 from pathlib import Path
 
-from pydantic.schema import schema
-
+from opentecr.data import CuratedMeasurement
 
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root / "src"))
 
-from opentecr.data import CuratedMeasurement
 
-
-def main():
+def main() -> None:
+    """Run the script."""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
     result_path = project_root / "output" / "openTECR.json"
-    result = json.dumps(
-        schema([CuratedMeasurement], title="openTECR Data Model"), indent=2
-    )
-    print(result)
+    result = json.dumps(CuratedMeasurement.model_json_schema(), indent=2)
+    logging.info(result)
     with result_path.open("w") as handle:
         handle.write(result)
 
